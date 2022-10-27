0917c45bf6fcf336e3f03c99c2af333191fffe2f
ElaniK <emili94kan@gmail.com>
2022-10-27 15:19:14 +0300

    работа над ДЗ модуля 3

--- NewsPaper/accounts/views.py
+++ NewsPaper/accounts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import ListView
+from .models import *
 
-# Create your views here.
+class AuthorList(ListView):
+    model = Author
+
+
+

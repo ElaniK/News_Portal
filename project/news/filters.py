96c5f1b112d588c64253b8aca37a315286825777
ElaniK <emili94kan@gmail.com>
2022-11-04 20:19:58 +0300

    Настроен поиск по дате

--- project/news/filters.py
+++ project/news/filters.py
@@ -1,12 +1,21 @@
-from django_filters import FilterSet
+from django_filters import FilterSet, DateTimeFilter
 from .models import *
+from django.forms import DateInput
+
 
 class PostFilter(FilterSet):
+    dateCreation = DateTimeFilter(
+        field_name='dateCreation',
+        lookup_expr='gt',
+        widget=DateInput(
+            format='%Y-%m-%d',
+            attrs={'type': 'date'}
+        ),
+    )
+
     class Meta:
         model = Post
         fields = {
-            'categoryType': ['iexact'],
+            'categoryType': ['exact'],
             'title': ['icontains'],
-            'dateCreation': ['date'],
         }
-
